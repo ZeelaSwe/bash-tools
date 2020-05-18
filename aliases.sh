6fd944324a79b0f1d28f5452579319d5905401c0
Mikael Holm <62749502+MickeHolm@users.noreply.github.com>
2020-05-18 10:54:34 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,5 @@ alias gitlg='git lg'
 alias gitlt='git log --all --decorate --oneline --graph'
 alias gits='git status'
 alias grep='grep --color=auto'
+
+git config --global alias.lg=log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'

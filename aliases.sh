2faa43509b0dea2ca7a65dba2b0770fa25f3637f
Mikael Holm <64324986+ZeelaSwe@users.noreply.github.com>
2020-05-18 10:47:54 +0200

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+alias gitb='git branch'
+alias gitl='git log'
+alias gitlg='git lg'
+alias gitlt='git log --all --decorate --oneline --graph'
+alias gits='git status'
+alias grep='grep --color=auto'

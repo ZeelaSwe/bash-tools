8b1da7993b130da020f6919bf0e23c6f9fc7af22
Mikael Holm <64324986+ZeelaSwe@users.noreply.github.com>
2020-05-25 09:07:17 +0200

    Fix git lg

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,6 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-git config --global alias.lg=log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'

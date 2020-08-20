49bb33616926c338c2de9199a1101ee3953e1899
Micke <mikael@zeela.se>
2020-08-20 20:58:31 +0200

    Added parameters to mnc & mnd

--- functions.sh
+++ functions.sh
@@ -1,5 +1,5 @@
 function dfind { grep --color=always -ir $1 | sort | less -R ; }
 function mcd { mkdir -p $1 && cd $1 ; }
 function gitbc { git rev-parse --abbrev-ref HEAD; }
-function mnc { cd /mnt/c ; }
-function mnd { cd /mnt/d ; }
+function mnc { cd "/mnt/c/$1" ; }
+function mnd { cd "/mnt/d//$1" ; }

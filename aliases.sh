46f1107a8d10a012810b7bbbd60b8e9d28d0b274
Mikael Holm <64324986+ZeelaSwe@users.noreply.github.com>
2026-09-14 11:09:07 +0200

    Add aliases for home and parent directory navigation
    Added new aliases for home directory navigation.

--- aliases.sh
+++ aliases.sh
@@ -14,6 +14,8 @@ alias grep='grep --color=auto'
 alias clgrep='grep --color=always'
 
 # Misc useful aliases
+alias home='cd ~'
+alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -22,3 +24,5 @@ alias .....='cd ../../../..'
 alias ll='ls -alF'
 alias lt='ls --human-readable --size -1 -S --classify'
 alias gh='history|grep'
+
+

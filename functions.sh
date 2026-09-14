0f207a6ac018ce8d1f0c84f8af4039a770dbef6d
Mikael Holm <64324986+ZeelaSwe@users.noreply.github.com>
2026-09-14 11:11:06 +0200

    Implement up function for directory navigation
    Added a function to navigate up a specified number of directories.

--- functions.sh
+++ functions.sh
@@ -24,4 +24,20 @@ function ext () {
    else
        echo "'$1' is not a valid file!"
    fi
- }
+}
+
+# Goes up a specified number of directories  (i.e. up 4)
+up ()
+{
+	local d=""
+	limit=$1
+	for ((i=1 ; i <= limit ; i++))
+		do
+			d=$d/..
+		done
+	d=$(echo $d | sed 's/^\///')
+	if [ -z "$d" ]; then
+		d=..
+	fi
+	cd $d
+}
